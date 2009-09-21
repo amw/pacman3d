9a61d4b1410e070cd5b9edc657788565f8d52161
Adam Wróbel <adam@fluxinc.ca>
2009-09-21 20:54:42 +0200

    Add doxygen documentation.

--- Makefile
+++ Makefile
@@ -14,5 +14,19 @@ clean:
 	rm -fr tmp
 	rm src/Makefile
 
+doc: doc/doxygen
+
+doc/doxygen: src/* doc/documentation.php doc/Doxyfile
+	@doxygen doc/Doxyfile
+	@echo "Html documentation updated."
+
+doc-pdf: doc/reference.pdf
+
+doc/reference.pdf: doc/doxygen
+	cp doc/latex-makefile doc/doxygen/latex/Makefile
+	cp doc/latex-missing-pspicture.ps doc/doxygen/latex/pspicture.ps
+	cd doc/doxygen/latex && $(MAKE)
+	cp doc/doxygen/latex/refman.pdf doc/reference.pdf
+
 force:
 
---
+++ doc/documentation.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @file doc/documentation.php
+ *
+ * @brief Documentation-only file.
+ *
+ * This file's only purpose is to gather documentation that should be used
+ * by Doxygen, but does not fit anywhere else.
+ */
+
+
+/**
+ * @mainpage
+ *
+ * @section introduction Introduction
+ *
+ * Pacman3d is a little pacman like game created in Qt/OpenGL. It's not meant to
+ * be big or great looking. I just need something playable to pass one of
+ * my classes on my University.
+ *
+ * @section author Author
+ *
+ * Adam Wróbel @n
+ * My login on github: amw @n
+ * Email: xellos ( at xellos info ) @n
+ *
+ */
+
+?>
